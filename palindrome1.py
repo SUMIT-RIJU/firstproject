51e6f4abdda9c7bf2aa344bd00b7c4157781968e
Sumit Saha <sumitsaha2205@gmail.com>
2020-10-02 11:28:28 +0530

    palindrome function

---
+++ palindrome1.py
@@ -0,0 +1,11 @@
+n=int(input("enter the no:"))
+temp=n
+rev=0
+while n>0:
+    d=n%10
+    rev=rev*10+d
+    n=n//10
+if temp==rev:
+    print("this is a palindromeno")
+else:
+    print("it is not a palindrome no")
